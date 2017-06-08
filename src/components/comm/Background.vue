5335bda8e2d513326e0e2e34b3e905f8f00724e2
tanrl <137988704@qq.com>
2017-06-08 15:04:56 +0800

    Background

--- src/components/comm/Background.vue
+++ src/components/comm/Background.vue
@@ -40,16 +40,22 @@
 
   #background-bg {
 <<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> dev
     min-width: 1054px;
     width: 100%; height: auto; min-height: 100%;
     background-image: url("../../assets/background/bg1.jpg");
     padding: 3rem 0;
     box-sizing: border-box;
+<<<<<<< HEAD
+=======
 =======
     min-width: 1000px;
     width: 100%;
     height: 100%;
     background-image: url("../../assets/background/bg1.jpg")
+>>>>>>> dev
 >>>>>>> dev
   }
 

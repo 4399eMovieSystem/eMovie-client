86c18c859df96da8dd413ca9996679c2621fbe82
chenwenhan <386190175@qq.com>
2017-06-09 01:16:52 +0800

    movie_list

--- src/components/pages/MovList/MovList.vue
+++ src/components/pages/MovList/MovList.vue
@@ -2,48 +2,48 @@
   <div>
     <div>
       <div id="title_font_1">{{ title1 }}</div>
-        <router-link :to="{ name: 'MovDetail', params: { mov_id: 2 } }">
-          <div id="post1" v-if="movie_detail">
-            <img class="image" v-bind:src="movie_detail.imgUrl"/>
+        <router-link :to="{ name: 'MovDetail', params: { mov_id: movie_list.now[0].mov_id } }">
+          <div id="post1" v-if="movie_list">
+            <img class="image" v-bind:src="movie_list.now[0].imgUrl"/>
           </div>
         </router-link>
-        <router-link :to="{ name: 'MovDetail', params: { mov_id: 2 } }">
-          <div id="post2" v-if="movie_detail">
-            <img class="image" v-bind:src="movie_detail.imgUrl"/>
+        <router-link :to="{ name: 'MovDetail', params: { mov_id: movie_list.now[1].mov_id  } }">
+          <div id="post2" v-if="movie_list">
+            <img class="image" v-bind:src="movie_list.now[1].imgUrl"/>
           </div>
         </router-link>
-        <router-link :to="{ name: 'MovDetail', params: { mov_id: 2 } }">
-          <div id="post3" v-if="movie_detail">
-            <img  class="image"v-bind:src="movie_detail.imgUrl"/>
+        <router-link :to="{ name: 'MovDetail', params: { mov_id: movie_list.now[2].mov_id  } }">
+          <div id="post3" v-if="movie_list">
+            <img  class="image"v-bind:src="movie_list.now[2].imgUrl"/>
           </div>
         </router-link>
-        <router-link :to="{ name: 'MovDetail', params: { mov_id: 2 } }">
-          <div id="post4" v-if="movie_detail">
-            <img class="image" v-bind:src="movie_detail.imgUrl"/>
+        <router-link :to="{ name: 'MovDetail', params: { mov_id: movie_list.now[3].mov_id  } }">
+          <div id="post4" v-if="movie_list">
+            <img class="image" v-bind:src="movie_list.now[3].imgUrl"/>
           </div>
         </router-link>
      
     </div>
     <div>
       <div id="title_font_2">{{ title2 }}</div>
-      <router-link :to="{ name: 'MovDetail', params: { mov_id: 2 } }">
-          <div id="post5" v-if="movie_detail">
-            <img  class="image" v-bind:src="movie_detail.imgUrl"/>
+      <router-link :to="{ name: 'MovDetail', params: { mov_id: movie_list.feature[0].mov_id } }">
+          <div id="post5" v-if="movie_list">
+            <img  class="image" v-bind:src="movie_list.feature[0].imgUrl"/>
           </div>
         </router-link>
-        <router-link :to="{ name: 'MovDetail', params: { mov_id: 2 } }">
-          <div id="post6" v-if="movie_detail">
-            <img class="image" v-bind:src="movie_detail.imgUrl"/>
+        <router-link :to="{ name: 'MovDetail', params: { mov_id: movie_list.feature[1].mov_id  } }">
+          <div id="post6" v-if="movie_list">
+            <img class="image" v-bind:src="movie_list.feature[1].imgUrl"/>
           </div>
         </router-link>
-        <router-link :to="{ name: 'MovDetail', params: { mov_id: 2 } }">
-          <div id="post7" v-if="movie_detail">
-            <img class="image" v-bind:src="movie_detail.imgUrl"/>
+        <router-link :to="{ name: 'MovDetail', params: { mov_id: movie_list.feature[2].mov_id  } }">
+          <div id="post7" v-if="movie_list">
+            <img class="image" v-bind:src="movie_list.feature[2].imgUrl"/>
           </div>
         </router-link>
-        <router-link :to="{ name: 'MovDetail', params: { mov_id: 2 } }">
-          <div id="post8" v-if="movie_detail">
-            <img class="image" v-bind:src="movie_detail.imgUrl"/>
+        <router-link :to="{ name: 'MovDetail', params: { mov_id: movie_list.feature[3].mov_id  } }">
+          <div id="post8" v-if="movie_list">
+            <img class="image" v-bind:src="movie_list.feature[3].imgUrl"/>
           </div>
         </router-link>
     </div>
@@ -51,53 +51,53 @@
       <div id="title_font_3">{{ title3 }}</div>
       <ul>
         <li>
-          <router-link :to="{ name: 'MovDetail', params: { mov_id: 2 } }">
-              <div id="movie1" v-if="movie_detail" class="rank_font">
-                {{movie_detail.name}}
+          <router-link :to="{ name: 'MovDetail', params: { mov_id: movie_list.rank[0].mov_id } }">
+              <div id="movie1" v-if="movie_list" class="rank_font">
+                {{movie_list.rank[0].name}}
               </div>
           </router-link>
           <div id="ticket1">
-            <h class="rank_font">2500万</h>
+            <h class="rank_font">{{movie_list.rank[0].grade}}</h>
           </div>
         </li>
         <li>
-          <router-link :to="{ name: 'MovDetail', params: { mov_id: 2 } }">
-              <div id="movie2" v-if="movie_detail" class="rank_font">
-                {{movie_detail.name}}
+          <router-link :to="{ name: 'MovDetail', params: { mov_id: movie_list.rank[1].mov_id } }">
+              <div id="movie2" v-if="movie_list" class="rank_font">
+                {{movie_list.rank[1].name}}
               </div>
           </router-link>
           <div id="ticket2">
-            <h class="rank_font">2500万</h>
+            <h class="rank_font">{{movie_list.rank[1].grade}}</h>
           </div>
         </li>
         <li>
-          <router-link :to="{ name: 'MovDetail', params: { mov_id: 2 } }">
-              <div id="movie3" v-if="movie_detail" class="rank_font">
-                {{movie_detail.name}}
+          <router-link :to="{ name: 'MovDetail', params: { mov_id: movie_list.rank[2].mov_id } }">
+              <div id="movie3" v-if="movie_list" class="rank_font">
+                {{movie_list.rank[2].name}}
               </div>
           </router-link>
           <div id="ticket3">
-            <h class="rank_font">2500万</h>
+            <h class="rank_font">{{movie_list.rank[2].grade}}</h>
           </div>
         </li>
         <li>
-          <router-link :to="{ name: 'MovDetail', params: { mov_id: 2 } }">
-              <div id="movie4" v-if="movie_detail" class="rank_font">
-                {{movie_detail.name}}
+          <router-link :to="{ name: 'MovDetail', params: { mov_id: movie_list.rank[3].mov_id } }">
+              <div id="movie4" v-if="movie_list" class="rank_font">
+                {{movie_list.rank[3].name}}
               </div>
           </router-link>
           <div id="ticket4">
-            <h class="rank_font">2500万</h>
+            <h class="rank_font">{{movie_list.rank[3].grade}}</h>
           </div>
         </li>
         <li>
-          <router-link :to="{ name: 'MovDetail', params: { mov_id: 2 } }">
-              <div id="movie5" v-if="movie_detail" class="rank_font">
-                {{movie_detail.name}}
+          <router-link :to="{ name: 'MovDetail', params: { mov_id: movie_list.rank[4].mov_id } }">
+              <div id="movie5" v-if="movie_list" class="rank_font">
+                {{movie_list.rank[4].name}}
               </div>
           </router-link>
           <div id="ticket5">
-            <h class="rank_font">2500万</h>
+            <h class="rank_font">{{movie_list.rank[4].grade}}</h>
           </div>
         </li>
       </ul>
@@ -113,15 +113,15 @@
       return {
         title1: '正在热映',
         title2: '即将上映',
-        title3: '今日票房',
-        movie_detail:null
+        title3: '今日榜单',
+        movie_list:null
       }
     },
     mounted:function() {
           this.movie_id = this.$route.path.substr(9);
-          getData({ apiKey: 'mov_info', params: { mov_id: 1 }  })
+          getData({ apiKey: 'mov_list'  })
           .then(response => {
-            this.movie_detail = response.data;
+            this.movie_list = response.data;
                      })
           .catch(err => {
             console.log('cin_mov err', err);
